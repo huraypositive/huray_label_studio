1555962a75f30668d0d7ac92905096b92dadd4c6
kyuhong0206 <gfvxgd2k@gmail.com>
2024-03-08 13:22:11 +0900

    add batch

--- src/data_src/crop_image.py
+++ src/data_src/crop_image.py
@@ -1,10 +1,11 @@
 import os
+from tabnanny import verbose
 import pandas as pd
 from PIL import Image
 from ultralytics import YOLO
 from glob import glob
 from tqdm import tqdm
-
+import time
 X1 = 0
 Y1 = 1
 X2 = 2
@@ -15,8 +16,9 @@ model.conf = 0.1
 
 image_dir = '/data3/crawl_data'
 output_dir = '/data3/crop_crawl_data'
-
+batch_size = 16
 df_item_list = []
+
 for food_name in tqdm(os.listdir(image_dir), desc="Processing food categories"):
     food_path = os.path.join(image_dir, food_name)
     if not os.path.exists(os.path.join(output_dir, food_name)):
@@ -24,26 +26,24 @@ for food_name in tqdm(os.listdir(image_dir), desc="Processing food categories"):
     if not os.path.isdir(food_path):
         continue
     files = [os.path.join(food_path, filename) for filename in os.listdir(food_path)]
-    for file in tqdm(files, desc = f'crop {food_name}'):
-        result = model(file, verbose=False)
-        bbox_list = result[0].boxes.xyxy.tolist()
-        for i, bbox_xyxy in enumerate(bbox_list):
-            if len(bbox_xyxy) == 0:
-                continue
-            file_name = os.path.basename(file).split('.')[0]
-            output_path = os.path.join(output_dir, food_name, f'{file_name}_{i}.jpg')
-            with Image.open(file) as img:
-                cropped_img = img.crop(bbox_xyxy)
-                cropped_img.convert('RGB').save(output_path)
-
-            #FIXME after analysis ratio
-            # width = bbox_xyxy[X2] - bbox_xyxy[X1]
-            # height = bbox_xyxy[Y2] - bbox_xyxy[Y1]
-            # if width/height > 10 or width/height < 0.01:
-            #     continue
-            df_item_list.append([file, output_path, food_name, bbox_xyxy])
+    for i in tqdm(range(0, len(files), batch_size), desc=f'Processing {food_name}'):
+        batch_files = files[i:i + batch_size]
+        results = model(batch_files, verbose = False)
+        for result in results:
+            bbox_list = result.boxes.xyxy.tolist()
+            file_name = os.path.basename(result.path).split('.')[0]
+            for j, bbox in enumerate(bbox_list):
+                if len(bbox) == 0:
+                    continue
+                output_path = os.path.join(output_dir, food_name, f'{file_name}_j.jpg')
+                df_item_list.append([result.path, output_path, food_name, bbox])
+                # with Image.open(result.path) as img:
+                #     cropped_img = img.crop(bbox)
+                #     cropped_img.convert('RGB').save(output_path)
 
 df = pd.DataFrame(df_item_list)
 df.columns['origin_image_path', 'item_path', 'food_name', 'bbox(xyxy)']
+df.to_csv('/home/ai04/workspace/huray_label_studio/data/output/meta_crop_image_data.csv')
+
 
 
