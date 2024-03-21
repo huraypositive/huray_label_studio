9f6fb9781d414b7bc10931d82075f943bcaf5adb
kyuhong0206 <gfvxgd2k@gmail.com>
2024-03-21 16:47:12 +0900

    add new user

--- src/app.py
+++ src/app.py
@@ -200,7 +200,7 @@ with gr.Blocks(head = shortcut_js, css = " .toast-wrap.svelte-pu0yf1 {top: 3%; l
         with gr.Column(scale=2):
             gr.Markdown("""# Huray Label Studio""")
             with gr.Row():
-                user_dropdown = gr.Dropdown(["hyunjoo", "jin", "kyuhong"], label = "user")
+                user_dropdown = gr.Dropdown(["hyunjoo", "jin", "jeonga"], label = "user")
                 work_check = gr.Checkbox(label="미작업 라벨만 보기")
             with gr.Row():
                 start_button = gr.Button('start', variant="primary")
