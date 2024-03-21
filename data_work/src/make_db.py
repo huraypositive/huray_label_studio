42f12c567545ea2e874e0666e14316758a838908
kyuhong0206 <gfvxgd2k@gmail.com>
2024-03-21 15:43:40 +0900

    something change..

--- data_work/src/make_db.py
+++ data_work/src/make_db.py
@@ -10,29 +10,28 @@ def make_dummy_db(db_path, json_path):
     with open(json_path, 'r', encoding = 'utf-8-sig') as f:
         json_data = json.load(f)
 
-    index = 0
-    for key in tqdm(json_data.keys()):
+    for index, key in enumerate(tqdm(json_data.keys())):
         for file_path in json_data[key]:
-            new_path = file_path.replace("/data/aihub", "/data3/aihub")
+            new_path = file_path.replace("/data/aihub", "/data3/aihub") #if use in v100 del this line
             db_dict = {"file_path": new_path, "class_name": key, "annotation": None}
             dict_bytes = pickle.dumps(db_dict)
             db[str(index).encode()] = dict_bytes
-            index += 1
 
     db.close()
 
-def make_user_index_db(user_index_db_path):
+def make_user_index_db(user_index_db_path, user_list):
     db = bdb.DB()
     db.open(user_index_db_path, None, bdb.DB_HASH, bdb.DB_CREATE)
-    db[b'hyunjoo'] = b'0'
-    db[b'jin'] = b'0'
-    db[b'kyuhong'] = b'0'
+    for user in user_list:
+        db[user.encode()] = b'0'
+        db[user.encode()] = b'0'
+        db[user.encode()] = b'0'
     db.close()
 
-def check_db():
+def check_db(user_index_db_path, user_name):
     db = bdb.DB()
-    db.open(f"/home/ai04/workspace/gradio_labeling/data/user_index.db", None, bdb.DB_HASH, bdb.DB_CREATE)
-    print(db[b'test'])
+    db.open(user_index_db_path, None, bdb.DB_HASH, bdb.DB_CREATE)
+    print(db[user_name.encode()])
 
 if __name__ == '__main__':
     user_list = ['hyunjoo', 'jin', 'kyuhong']
@@ -41,4 +40,4 @@ if __name__ == '__main__':
     user_index_db_path = "/home/ai04/workspace/huray_label_studio/data/user_index.db"
     for db_path in db_path_list:
         make_dummy_db(db_path, json_path)
-    make_user_index_db(user_index_db_path)
+    make_user_index_db(user_index_db_path, user_list)
