52005e41be5bf65053c1052a85821ce2c0d1e4da
kyuhong0206 <gfvxgd2k@gmail.com>
2024-04-01 15:23:20 +0900

    add analysis date

--- src/admin.py
+++ src/admin.py
@@ -5,13 +5,51 @@ import os
 import pickle
 import matplotlib.pyplot as plt
 from matplotlib import font_manager, rc
-
-DBPATH = '/data3/food_labelingDB'
-CFGHEIGHT = 600
+from gradio_calendar import Calendar
+from utils import get_db_connection
 
 font_path = "/usr/share/fonts/truetype/nanum/NanumBarunGothic.ttf"
 font_prop = font_manager.FontProperties(fname=font_path)
 rc('font', family=font_prop.get_name())
+
+def get_db(user_list):
+    data_list = []
+    for user in user_list:
+        db = get_db_connection(user)
+        for key in db.keys():
+            data_bytes = db.get(key)
+            data_list.append(pickle.loads(data_bytes))
+    return data_list
+
+def analysis_all_date(user_list):
+    df = pd.DataFrame(get_db(user_list))
+    df['annotation'] = df['annotation'].apply(lambda x: 'Empty' if x == None else x)
+    count_df = df[df['annotation'].notnull()]['annotation'].value_counts()
+    total = sum(count_df.values)
+    percentages = [(count / total) * 100 for count in count_df]
+    legend_labels = [f'{label}: {percentage:.1f}%' for label, percentage in zip(count_df.index, percentages)]
+    fig, ax = plt.subplots()
+    ax.pie(count_df, startangle=140, wedgeprops=dict(width=0.3))
+    ax.legend(legend_labels, title="Annotations", loc="best")
+    ax.set_title(f'{user_dropdown} Annotation Distribution')
+
+    return fig, f"{count_df.get('True', 0)} ({count_df.get('True', 0) / total*100:.2f}%)", f"{count_df.get('False', 0)} ({count_df.get('False', 0) / total*100:.2f}%)", f"{count_df.get('unknown', 0)} ({count_df.get('unknown', 0) / total*100:.2f}%)", f"{count_df.get('Empty', 0)} ({count_df.get('Empty', 0) / total*100:.2f}%)", total, int(total) - int(count_df.get('Empty', 0))
+
+def analysis_each_date(user_list, date_time):
+    date = date_time.strftime("%Y-%m-%d")
+    df = pd.DataFrame(get_db(user_list))
+    filtered_df = df[df['datetime'] == date]
+    filtered_df['annotation'] = filtered_df['annotation'].apply(lambda x: 'Empty' if x == None else x)
+    count_df = filtered_df[filtered_df['annotation'].notnull()]['annotation'].value_counts()
+    total = sum(count_df.values)
+    percentages = [(count / total) * 100 for count in count_df]
+    legend_labels = [f'{label}: {percentage:.1f}%' for label, percentage in zip(count_df.index, percentages)]
+    fig, ax = plt.subplots()
+    ax.pie(count_df, startangle=140, wedgeprops=dict(width=0.3))
+    ax.legend(legend_labels, title="Annotations", loc="best")
+    ax.set_title(f'{user_dropdown} Annotation Distribution')
+
+    return fig, f"{count_df.get('True', 0)} ({count_df.get('True', 0) / total*100:.2f}%)", f"{count_df.get('False', 0)} ({count_df.get('False', 0) / total*100:.2f}%)", f"{count_df.get('unknown', 0)} ({count_df.get('unknown', 0) / total*100:.2f}%)", f"{count_df.get('Empty', 0)} ({count_df.get('Empty', 0) / total*100:.2f}%)", total, int(total) - int(count_df.get('Empty', 0))
 
 
 def analysis_all(user_dropdown):
@@ -77,9 +115,13 @@ with gr.Blocks(theme = gr.themes.Soft()) as demo:
                 plot_output = gr.Plot()
         with gr.Column(scale=2):
             with gr.Row():
-                user_dropdown = gr.Dropdown(["hyunjoo_20240326", "jin_20240326", "jeonga_20240326"], label = "user")
+                user_list = gr.CheckboxGroup(["hyunjoo", "jin", "jeonga", "mijeong"], label = "user")
             with gr.Row():
-                start_button = gr.Button('전체 조회', variant="primary")
+                date_time = Calendar(type="datetime", label="Select a date", info="Click the calendar icon to bring up the calendar.")
+            with gr.Row():
+                date_button = gr.Button('날짜별 조회')
+                all_date_button = gr.Button('전체 날짜 조회', variant="primary")
+            with gr.Row():
                 class_text = gr.Textbox(label = 'class', max_lines = 1)
                 index_button = gr.Button('클래스 조회')
                 true_count_text = gr.Textbox(label = 'true count', interactive = False, max_lines = 1)
@@ -90,7 +132,8 @@ with gr.Blocks(theme = gr.themes.Soft()) as demo:
                 work_count_text = gr.Textbox(label = 'work count', interactive = False, max_lines = 1)
                 toal_count_text = gr.Textbox(label = 'total count', interactive = False, max_lines = 1)
   
-    start_button.click(analysis_all, inputs = [user_dropdown], outputs = [plot_output, true_count_text, false_count_text, unknown_count_text, none_count_text,toal_count_text,work_count_text])
+    all_date_button.click(analysis_all_date, inputs = [user_list], outputs = [plot_output, true_count_text, false_count_text, unknown_count_text, none_count_text,toal_count_text,work_count_text])
+    date_button.click(analysis_each_date, inputs = [user_list, date_time], outputs = [plot_output, true_count_text, false_count_text, unknown_count_text, none_count_text,toal_count_text,work_count_text])
     index_button.click(cate_annotation_chart, inputs = [user_dropdown, class_text], outputs = [plot_output, true_count_text, false_count_text, unknown_count_text, none_count_text,toal_count_text,work_count_text])
 
 demo.launch(ssl_verify=False, share=True, server_name="0.0.0.0")
